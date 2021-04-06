397744a2928a9fc88e50a17199fd2fa31a9a3ffc
tuxfy <social.tux@posteo.de>
2021-04-06 17:10:06 +0200

    adds swipe functionality + bigger mobile indicator

--- src/components/Carousel/Control/control.component.jsx
+++ src/components/Carousel/Control/control.component.jsx
@@ -1,32 +1,13 @@
-import React, { useContext } from 'react'
 import './control.styles.scss'
 
-import QuestionContext from '../../../context/question.context'
-
-export default function Control({ direction }) {
-	let [context, setContext] = useContext(QuestionContext)
-	let { questions, currentQuestion } = context
-
-	const navigate = el => {
-		const count = context.questions.length
+export default function Control({ direction, navFunc }) {
+	const handleOnClick = el => {
 		const direction = el.target.dataset.direction
-		let index = currentQuestion.index
-
-		if (direction === `left`) {
-			index = index - 1 < 0 ? count - 1 : index - 1
-		} else {
-			index = index + 1 === count ? 0 : index + 1
-		}
-		
-		const newContext = {
-			...context,
-			currentQuestion: { ...questions[index], index: index },
-		}
-		setContext(newContext)
+		navFunc(direction)
 	}
 
 	return (
-		<div className="control" data-direction={direction} onClick={navigate}>
+		<div className="control" data-direction={direction} onClick={handleOnClick}>
 			<div className={`arrow ${direction}`}></div>
 			<div className={`arrow ${direction}`}></div>
 		</div>
--- src/components/Carousel/carousel.component.jsx
+++ src/components/Carousel/carousel.component.jsx
@@ -11,21 +11,66 @@ import QuestionContext from '../../context/question.context'
 export default function Carousel() {
 	questionData.currentQuestion = { ...questionData.questions[0], index: 0 }
 	const [context, setContext] = useState(questionData)
+	let { questions, currentQuestion } = context
+	const [touchStart, setTouchStart] = useState(0)
+	const [touchEnd, setTouchEnd] = useState(0)
+
+	const handleTouchStart = e => {
+		setTouchStart(e.targetTouches[0].clientX)
+	}
+
+	const handleTouchMove = e => {
+		setTouchEnd(e.targetTouches[0].clientX)
+	}
+
+	const handleTouchEnd = () => {
+		if (touchStart - touchEnd > 150) {
+			navigate(`right`)
+		}
+
+		if (touchStart - touchEnd < -150) {
+			navigate(`left`)
+		}
+	}
+
+	const navigate = direction => {
+		const count = context.questions.length
+		let index = currentQuestion.index
+
+		if (direction === `left`) {
+			index = index - 1 < 0 ? count - 1 : index - 1
+		} else {
+			index = index + 1 === count ? 0 : index + 1
+		}
+
+		const newContext = {
+			...context,
+			currentQuestion: { ...questions[index], index: index },
+		}
+		setContext(newContext)
+	}
 
 	return (
 		<QuestionContext.Provider value={[context, setContext]}>
 			<div
-				className="carouselHeader"
-				style={{
-					backgroundImage: `url(${window.location}/images/${context.currentQuestion.image})`,
-				}}
+				className="questionCarousel"
+				onTouchStart={handleTouchStart}
+				onTouchMove={handleTouchMove}
+				onTouchEnd={handleTouchEnd}
 			>
-				<Control direction="left" />
-				<Control direction="right" />
-			</div>
-			<div className="carouselContent">
-				<Form />
-				<Indicator />
+				<div
+					className="carouselHeader"
+					style={{
+						backgroundImage: `url(${window.location}/images/${context.currentQuestion.image})`,
+					}}
+				>
+					<Control direction="left" navFunc={navigate} />
+					<Control direction="right" navFunc={navigate} />
+				</div>
+				<div className="carouselContent">
+					<Form />
+					<Indicator />
+				</div>
 			</div>
 		</QuestionContext.Provider>
 	)
